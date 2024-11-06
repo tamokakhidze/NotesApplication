bceeda1d465ed3a45fb6c2fd9d72e341c79237a6
Tamo <tamuna.kakhidze.1@btu.edu.ge>
2024-11-06 12:13:59 +0400

    Refactor NoteCell: Modularize setup, use lazy properties, and improve initialization

--- NotesApp/Customs/NoteCell.swift
+++ NotesApp/Customs/NoteCell.swift
@@ -10,59 +10,77 @@ import CoreData
 
 final class NoteCell: UITableViewCell {
     
-    var titleLabel = UILabel()
-    var deleteButton = UIButton()
+    //MARK: - Properties
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        label.numberOfLines = Sizing.NoteCell.titleNumberOfLines
+        label.font = .systemFont(ofSize: Sizing.NoteCell.titleFontSize)
+        return label
+    }()
+    
+    private lazy var deleteButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(systemName: "trash"), for: .normal)
+        button.backgroundColor = .darkGray
+        button.layer.cornerRadius = Sizing.Button.cellDeleteButtonCornerRadius
+        button.tintColor = .white
+        button.addTarget(self, action: #selector(deleteButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
     var deleteAction: (() -> Void)?
     
+    //MARK: - Init
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        titleLabel = configureTitleLabel()
-        deleteButton = configureDeleteButton()
-        contentView.layer.cornerRadius = Sizing.NoteCell.cornerRadius
-        contentView.backgroundColor = .cell
-        sendSubviewToBack(contentView)
-        contentView.clipsToBounds = true
+        initialise()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func configureTitleLabel() -> UILabel {
-        let titleLabel = UILabel()
-        addSubview(titleLabel)
-        titleLabel.textAlignment = .left
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.textColor = .black
-        titleLabel.heightAnchor.constraint(equalToConstant: Sizing.NoteCell.titleHeight).isActive = true
-        titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
-        titleLabel.widthAnchor.constraint(lessThanOrEqualToConstant: Sizing.NoteCell.titleWidth).isActive = true
-        titleLabel.numberOfLines = Sizing.NoteCell.titleNumberOfLines
-        titleLabel.font = .systemFont(ofSize: Sizing.NoteCell.titleFontSize)
-        return titleLabel
+    //MARK: - Setup Methods
+    private func initialise() {
+        setupUI()
+        setupHierarchy()
+        setConstraints()
     }
     
-    private func configureDeleteButton() -> UIButton {
-        let deleteButton = UIButton()
-        addSubview(deleteButton)
-        deleteButton.translatesAutoresizingMaskIntoConstraints = false
-        deleteButton.heightAnchor.constraint(equalToConstant: Sizing.Button.cellDeleteButtonHeight).isActive = true
-        deleteButton.widthAnchor.constraint(equalToConstant: Sizing.Button.cellDeleteButtonHeight).isActive = true
-        deleteButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: Sizing.NoteCell.deleteButtonAnchor).isActive = true
-        deleteButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: Sizing.NoteCell.deleteButtonAnchor).isActive = true
-        deleteButton.setImage(UIImage(systemName: "trash"), for: .normal)
-        deleteButton.backgroundColor = .darkGray
-        deleteButton.layer.cornerRadius = Sizing.Button.cellDeleteButtonCornerRadius
-        deleteButton.tintColor = .white
-        
-        deleteButton.addTarget(self, action: #selector(deleteButtonTapped), for: .touchUpInside)
-        return deleteButton
+    private func setupUI() {
+        contentView.layer.cornerRadius = Sizing.NoteCell.cornerRadius
+        contentView.backgroundColor = .cell
+        sendSubviewToBack(contentView)
+        contentView.clipsToBounds = true
     }
     
+    private func setupHierarchy() {
+        addSubviews(titleLabel, deleteButton)
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            titleLabel.heightAnchor.constraint(equalToConstant: Sizing.NoteCell.titleHeight),
+            titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            titleLabel.widthAnchor.constraint(lessThanOrEqualToConstant: Sizing.NoteCell.titleWidth),
+            
+            deleteButton.heightAnchor.constraint(equalToConstant: Sizing.Button.cellDeleteButtonHeight),
+            deleteButton.widthAnchor.constraint(equalToConstant: Sizing.Button.cellDeleteButtonHeight),
+            deleteButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: Sizing.NoteCell.deleteButtonAnchor),
+            deleteButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: Sizing.NoteCell.deleteButtonAnchor)
+        ])
+    }
+    
+    //MARK: - Actions
     @objc func deleteButtonTapped() {
         deleteAction?()
     }
     
+    //MARK: - Cell Configuration
     func configureCell(title: String) {
         titleLabel.text = title
     }
