14696e99f4883ba18bb148eea269e95087a5cdbd
Tamo <tamuna.kakhidze.1@btu.edu.ge>
2024-11-06 11:48:13 +0400

    Modify BackButton class: Add marks, create setConstraints method for separation of concerns

--- NotesApp/Customs/BackButton.swift
+++ NotesApp/Customs/BackButton.swift
@@ -8,19 +8,38 @@
 import UIKit
 
 final class BackButton: UIButton {
-
+    
+    //MARK: - Properties
+    private var width: CGFloat
+    private var height: CGFloat
+    
+    //MARK: - Init
     init(width: CGFloat, height: CGFloat, backgroundImage: String = "", backgroundColor: UIColor) {
+        self.width = width
+        self.height = height
         super.init(frame: .zero)
-        self.translatesAutoresizingMaskIntoConstraints = false
-        self.widthAnchor.constraint(equalToConstant: width).isActive = true
-        self.heightAnchor.constraint(equalToConstant: height).isActive = true
         self.backgroundColor = backgroundColor
         self.layer.cornerRadius = Sizing.Button.backButtonCornerRadius
         self.setImage(UIImage(named: backgroundImage), for: .normal)
+        
+        setupUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
+    //MARK: - Ui Setup
+    private func setupUI() {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        setConstraints()
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            self.widthAnchor.constraint(equalToConstant: width),
+            self.heightAnchor.constraint(equalToConstant: height)
+        ])
+    }
 }
